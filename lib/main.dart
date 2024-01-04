fe013315e7cbeeea8d25a83e83ef3c84aad07a2b
rayhan <rayhanzz772@gmail.com>
2024-01-04 18:55:57 +0700

    new update

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Firebase Auth Demo',
+      title: 'Separaga',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Droid Sans',
